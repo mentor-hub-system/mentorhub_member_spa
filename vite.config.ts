71237c5bbaa1aba3c0caea762affadd0591b42b6
Stage0 Launch <stage0-launch@localhost>
2026-03-27 03:28:30 +0000

    Template Merge Processing Complete

--- vite.config.ts
+++ vite.config.ts
@@ -10,13 +10,12 @@ export default defineConfig({
     }
   },
   server: {
-    port: {{repo.port}},
+    port: 8392,
     proxy: {
       '/api': {
-        target: 'http://localhost:{{repo.port - 1}}',
+        target: 'http://localhost:8391',
         changeOrigin: true
       }
     }
   }
 })
-
